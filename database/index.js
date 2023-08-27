c4af030ccc7f3fd051d98afe839e485d9de2705d
huziibee <124780347+huziibee@users.noreply.github.com>
2023-08-27 21:35:16 +0200

    added MONGOURL

--- database/index.js
+++ database/index.js
@@ -4,3 +4,5 @@ mongoose
   .connect( procesprocess.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch((err) => console.error('Error connecting to MongoDB Atlas:', err));
+
+  // mongodb url added 
